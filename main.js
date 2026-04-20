913ea6aa3383b27f4bd30a60c942f1ecb0553163
Misha Manalo <manalo.mb@obmontessori.edu.ph>
2026-04-21 07:29:42 +0800

    main
    submit to main

---
+++ main.js
@@ -0,0 +1,9 @@
+function checkGate() {
+    let carCounter = 18;//initial count
+    const maxCars = 25;
+
+    while (carCounter < maxCars){
+        window.alert(carCounter);
+        carCounter += 1;
+    }
+}
